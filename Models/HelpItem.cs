63f778f2b87af13ba73b9c094af79474174a1d7a
Leonardo <leo.pinnheiro@gmail.com>
2025-06-26 12:23:46 -0300

    Automatic set helpwindow shortcuts.

--- Models/HelpItem.cs
+++ Models/HelpItem.cs
@@ -1,9 +1,21 @@
-﻿namespace nightreign_auto_storm_timer.Models
+﻿using nightreign_auto_storm_timer.Enums;
+using nightreign_auto_storm_timer.Utils;
+
+namespace nightreign_auto_storm_timer.Models
 {
     public class HelpItem
     {
-        public string Command { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public ShortcutAction Action { get; }
+        public string DisplayName => EnumHelper.GetDescription(Action);
+
+        private Shortcut Shortcut;
+        public string DisplayCommand => Shortcut.ToFormattedString();
+
+        public HelpItem(ShortcutAction action, ShortcutConfig config)
+        {
+            Action = action;
+            Shortcut = config.Shortcut;
+        }
     }
 
 }
--- ViewModels/HelpViewModel.cs
+++ ViewModels/HelpViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using nightreign_auto_storm_timer.Enums;
+using nightreign_auto_storm_timer.Managers;
 using nightreign_auto_storm_timer.Models;
 using System.Collections.ObjectModel;
 
@@ -10,15 +12,27 @@ public partial class HelpViewModel : ObservableObject
 
     public HelpViewModel()
     {
-        HelpList.Add(new HelpItem { Command = "Ctrl + Q", Description = "Close the application" });
-        HelpList.Add(new HelpItem { Command = "F1", Description = "Start, pause, and advance the phase" });
-        HelpList.Add(new HelpItem { Command = "F2", Description = "Reset the remaining time" });
-        HelpList.Add(new HelpItem { Command = "F3", Description = "Go to the previous phase" });
-        HelpList.Add(new HelpItem { Command = "F4", Description = "Go to the next phase" });
-        HelpList.Add(new HelpItem { Command = "F5", Description = "Toggle compact mode" });
-        HelpList.Add(new HelpItem { Command = "F6", Description = "Toggle automatic mode" });
-        HelpList.Add(new HelpItem { Command = "F7", Description = "Show/hide the help window" });
-        HelpList.Add(new HelpItem { Command = "F8", Description = "Show/hide the configuration window" });
-        HelpList.Add(new HelpItem { Command = "F9", Description = "If DEBUG is enabled, take a DEBUG screenshot of the screen." });
+        AppConfigManager.Instance.RegisterForUpdates(OnConfigUpdated);
+        ConfigFields(AppConfigManager.Instance.CurrentConfig.Shortcuts);
+    }
+
+    private void OnConfigUpdated(AppConfigNew config)
+    {
+        ConfigFields(config.Shortcuts);
+    }
+
+    public void ConfigFields(Dictionary<string, ShortcutConfig> shortcutsConfig)
+    {
+        HelpList.Clear();
+        foreach (ShortcutAction action in Enum.GetValues(typeof(ShortcutAction)))
+        {
+            if (shortcutsConfig.TryGetValue(action.ToString(), out var shortcutConfig))
+            {
+                if (shortcutConfig.Enabled)
+                {
+                    HelpList.Add(new HelpItem(action, shortcutConfig));
+                }
+            }
+        }
     }
 }
